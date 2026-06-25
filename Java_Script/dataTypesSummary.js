e46e673f9d0c3c7699836134603e502879ccdddf
Shiv-dev2807 <shivprasaddevkatte2807@gmail.com>
2026-06-25 20:28:39 +0530

    js

--- Java_Script/dataTypesSummary.js
+++ Java_Script/dataTypesSummary.js
@@ -33,3 +33,30 @@ console.log(typeof heros); // object
 console.log(typeof person); // object
 console.log(typeof id); // symbol
 console.log(typeof null); // object, this is a bug in JavaScript, null is a primitive data type but typeof returns object.
+
+//Memories
+// Stack(primitive types) and Heap(reference types)(non-primitive)
+
+
+let name = "John"; // stored in stack memory
+let anotherName = name; // stored in stack memory, anotherName is a copy of name
+
+anotherName = "Doe"; // changing the value of anotherName, does not affect name
+
+console.log(name); // John
+console.log(anotherName); // Doe
+
+let person1 = {
+    name: "John",
+    age: 30,
+} // stored in heap memory
+
+let anotherPerson = person1; // stored in stack memory, anotherPerson is a reference to person1
+
+anotherPerson.age = 31; // changing the value of anotherPerson, affects person1
+
+console.log(person1.age); // 31
+console.log(anotherPerson.age); // 31
+
+//ok so in sumamry, primitive data types are stored in stack memory and are immutable, while reference data types are stored in heap memory and are mutable.
+//also means that stack sends a copy of the value to another variable, while heap sends a reference to the same object in memory.
