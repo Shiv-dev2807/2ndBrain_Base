28aae18853d770027d5bea4790f48cf3bec59089
Shiv-dev2807 <shivprasaddevkatte2807@gmail.com>
2026-06-28 07:26:21 +0530

    js

--- Java_Script/06_dom/one.js
+++ Java_Script/06_dom/one.js
@@ -12,4 +12,65 @@ console.log(document.links[2]);
 document.getElementById("firstHeading")
 document.getElementById("firstHeading").innerHTML="<h1>Shiv</h1>"
 
+
+
+
+tempList.forEach( (l) => {
+    l.style.color="blue"
+})
+
+
+
+
+document.getElementsByClassName
+
+
+
+
+html collection to array
+Array.from(tempList)
+
+log.js:79 Use of "mw.eventLog.eventInSample" is deprecated. Use "mw.eventLog.pageviewInSample()" instead.
+maybeLog @ log.js:79
+log.js:79 Use of "mw.eventLog.eventInSample" is deprecated. Use "mw.eventLog.pageviewInSample()" instead.
+maybeLog @ log.js:79
+document.querySelectorAll('h2')
+NodeList(5) [h2.vector-pinnable-header-label, h2#Early_life, h2#Career, h2#References, h2#External_links]
+const allH2 = document.querySelectorAll('h2')
+undefined
+allH2[0].innerText
+''
+allH2[1].innerText
+'Early life'
+document.querySelectorAll('.mw_headline')
+NodeList []length: 0[[Prototype]]: NodeList
+myH2[0].innerText
+VM1022:1 Uncaught ReferenceError: myH2 is not defined
+    at <anonymous>:1:1
+(anonymous) @ VM1022:1
+allH2[0].innerText
+''
+allH2.forEach( () => {} )
+undefined
+allH2.forEach( (h) => {
+    h.style.color="red"
+} )
+undefined
+allH2.forEach( (h) => {
+    h.style.color="red";
+    h.style.backgrounColor="green";
+    h.style.padding="10px";
+    h.innerText ="shiv"
+} )
+undefined
 */
+
+//=================================================================================
+
+//textContent = displays all hidden text also //! dom learning on chai and code test test
+//innerText = only visible text //! dom learning on chai and code
+//innerHTML = gives inner HTML value if written //! dom learning on chai and code <span style="display:none;">"test test"</span>
+
+//querySelector
+
+
