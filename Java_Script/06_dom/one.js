4f64acaf12d320bbcdf9de06aeb77438a1d519e0
Shiv-dev2807 <shivprasaddevkatte2807@gmail.com>
2026-06-28 06:43:55 +0530

    js

---
+++ Java_Script/06_dom/one.js
@@ -0,0 +1,15 @@
+//! in inspect console
+/*
+
+console.log(window);
+console.log(window.document);
+console.log(document);
+console.dir(document);
+console.log(document.location);
+console.log(document.links); // html collections
+console.log(document.links[2]);
+
+document.getElementById("firstHeading")
+document.getElementById("firstHeading").innerHTML="<h1>Shiv</h1>"
+
+*/
