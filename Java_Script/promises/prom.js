6fadae296906b27b50fded70cdded252e1cd360b
Shiv-dev2807 <shivprasaddevkatte2807@gmail.com>
2026-07-25 19:06:34 +0530

    learning js

--- Java_Script/promises/prom.js
+++ Java_Script/promises/prom.js
@@ -1,6 +1,6 @@
 // consume mostly
 
-const { use } = require("react");
+
 
 // fetch('http://sss.com').then().catch().finally()
 
@@ -53,3 +53,28 @@ const promiseThree = new Promise(function(resolve,reject){
 promiseThree.then(function(user){
     console.log(user);
 })
+
+
+const promiseFour = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let error = false
+        if(!error){
+            resolve({username:"Shiv",password:"123"})
+        }else{
+            reject('ERROR Something went wrong')
+        }
+    },1000)
+})
+
+// promiseFour.then().catch()
+
+promiseFour.then((user)=>{
+    console.log(user);
+    return user.username
+}).then((username)=>{
+    console.log(username);
+}).catch(function(error){
+    console.log(error);  
+})
+
+
